43552c49ffb6ec1b29b47172e300a88637e38e3f
Ellinor Stenhaug <ellinorstenhaug@Ellinors-MacBook-Air.local>
2018-10-03 11:27:23 +0200

    lektion 4

---
+++ Lektion-4/main.js
@@ -0,0 +1,8 @@
+document.write("Hello World");
+let el1 = document.getElementById("demo1");
+el1.innerHTML = "<h1>Demo 1</h1>";
+
+let el2 = document.getElementById("demo2")
+el2.textContent = "Lite text och annat";
+
+// document.getElementById("heading").style.color="green";
